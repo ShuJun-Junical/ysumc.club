338d028301bfd2423d1085a2490037213662f8f0
纾浚 <junical@email.cn>
2023-09-06 17:30:14 +0800

    项目页大致完工

--- components/project/ProjectCard.vue
+++ components/project/ProjectCard.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="bg-white flex items-center">
+  <div class="bg-white flex items-center py-16 px-10">
     <div>
-      <h2>{{ props.title }}</h2>
-      <p>{{ props.subtitle || ' ' }}</p>
-      <p>{{ props.time || ' ' }}</p>
-      <NuxtLink :to="!props.state ? props.link : '/project'">{{
-        ['了解更多', 'WIP', '已写入计划'][props.state]
-      }}</NuxtLink>
+      <h2 class="text-4xl pb-2 leading-normal">
+        <strong v-html="props.title"></strong>
+      </h2>
+      <p class="text-lg py-2">{{ props.subtitle || ' ' }}</p>
+      <p class="text-lg py-4 mb-8">{{ props.time || ' ' }}</p>
+      <NuxtLink
+        :to="props.state === 0 || props.state === 1 ? props.link : ''"
+        class="transition-colors py-3 px-6 rounded text-center text-lg border-2"
+        :class="
+          [
+            'border-cyan-500 hover:border-cyan-600 text-cyan-500 hover:text-cyan-600',
+            'border-pink-500 text-pink-500 hover:border-pink-600 hover:text-pink-600',
+            'border-gray-500 text-gray-500',
+          ][props.state]
+        "
+        >{{ ['了解更多', 'WIP', '已写入计划'][props.state] }}</NuxtLink
+      >
     </div>
-    <NuxtImg :src="props.image" class="w-1/2" />
+    <NuxtImg :src="props.image" class="aspect-video w-1/2 ml-auto" />
   </div>
 </template>
 <script setup lang="ts">
--- pages/project/index.vue
+++ pages/project/index.vue
@@ -7,12 +7,146 @@
       >这里是项目页，下滑以了解更多项目。</PublicCountdown
     >
   </PublicHero>
-  <PublicCenterText title="工程项目">来看看我们的本事吧。</PublicCenterText>
-  <ProjectCard
-    title="项目1"
-    subtitle="项目1的副标题"
-    time="2021/9/1"
-    image="/img/Project/Hero.png"
-    class="max-w-7xl mx-auto"
-  />
+  <div
+    v-for="(i, key) in project"
+    :key="i.title"
+    :class="{ 'mb-16': key === project.length - 1 }"
+  >
+    <PublicCenterText :title="i.title">{{ i.desc }}</PublicCenterText>
+    <ProjectCard
+      v-for="j in i.content"
+      :key="j.title"
+      :title="j.title"
+      :subtitle="j.subtitle"
+      :time="j.time"
+      :state="j.state"
+      :image="j.image"
+      :link="j.link"
+      class="max-w-7xl mx-auto my-8"
+    />
+  </div>
 </template>
+<script setup lang="ts">
+const project = [
+  {
+    title: '工程项目',
+    desc: '来看看我们的本事吧。',
+    content: [
+      {
+        title: ' 像素燕大<br />《燕山大学复原计划》',
+        subtitle: '——三千一百万方块打造的虚拟校园。',
+        time: '2022.4-2022.12',
+        state: 0,
+        image: '/img/Project/Work-1.png',
+        link: '/project/work/pixel-ysu',
+      },
+      {
+        title: '《云游燕大》学校导览系统',
+        subtitle: '——游戏中的燕大魅力丝毫不减。',
+        time: '',
+        state: 1,
+        image: '/img/Project/Work-2.png',
+        link: '/project/work/online-ysu',
+      },
+      {
+        title: '一个大工程',
+        subtitle: '',
+        time: '预计于2023.9完成',
+        state: 1,
+        image: '/img/Project/WIP.png',
+        link: '',
+      },
+      {
+        title: '《定格之中》虚拟校史馆',
+        subtitle: '——走进照片，回首百年。',
+        time: '',
+        state: 2,
+        image: '/img/Project/WIP.png',
+        link: '',
+      },
+    ],
+  },
+  {
+    title: '活动',
+    desc: '用欢笑打破距离的隔阂。',
+    content: [
+      {
+        title: '时空悖论 ：劫后启示录',
+        subtitle: '——在逆境中重燃希望。',
+        time: '2023.1.6  已结束',
+        state: 0,
+        image: '/img/Project/Activity-1.png',
+        link: '/project/activity/rpg-skbl-jhqsl',
+      },
+      {
+        title: '玩家联谊会',
+        subtitle: '——每逢佳节便会到来的欢庆。',
+        time: '节日期间举行',
+        state: 0,
+        image: '/img/Project/Activity-2.png',
+        link: '/project/activity/festival-party',
+      },
+      {
+        title: '100分钟建筑赛',
+        subtitle: '——能力与智谋的较量。',
+        time: '不定期举行',
+        state: 0,
+        image: '/img/Project/Activity-3.png',
+        link: '/project/activity/100min-construct',
+      },
+    ],
+  },
+  {
+    title: '工作流',
+    desc: '专业能力永远很重要。',
+    content: [
+      {
+        title: '3D打印工作流',
+        subtitle: '——虚拟成真的纪念。',
+        time: '',
+        state: 0,
+        image: '/img/Project/Workflow-1.png',
+        link: '/project/workflow/3d-print',
+      },
+      {
+        title: '视效工作流',
+        subtitle: '——让一切真实与生动。',
+        time: '',
+        state: 0,
+        image: '/img/Project/Workflow-2.png',
+        link: '/project/workflow/visual-effect',
+      },
+      {
+        title: '正式社员培训工作流',
+        subtitle: '——薪火相传的意义。',
+        time: '',
+        state: 0,
+        image: '/img/Project/Workflow-3.png',
+        link: '/project/workflow/member-training',
+      },
+    ],
+  },
+  {
+    title: '多人游戏与工作平台',
+    desc: '你并不孤独。',
+    content: [
+      {
+        title: '燕山大学服务器',
+        subtitle: '——为燕山大学Minecraft爱好者搭建起的平台。',
+        time: '',
+        state: 0,
+        image: '/img/Project/MCServer-1.png',
+        link: '/project/mcserver/ysu-server',
+      },
+      {
+        title: '《原初燕大服》',
+        subtitle: '——这个不堪一击的服务器是一切的原点。',
+        time: '2022.2  已关闭',
+        state: 0,
+        image: '/img/Project/MCServer-2.png',
+        link: '/project/mcserver/old-server',
+      },
+    ],
+  },
+]
+</script>
