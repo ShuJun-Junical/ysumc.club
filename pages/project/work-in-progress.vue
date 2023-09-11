96da6bfee092278f47da5283b13dc2ffcd73dd95
纾浚 <junical@email.cn>
2023-09-11 13:50:30 +0800

    防止在 wip 页面点击返回按钮时启动原神

--- pages/project/work-in-progress.vue
+++ pages/project/work-in-progress.vue
@@ -3,7 +3,7 @@
     <div class="absolute bottom-36 left-10">
       <h1 class="text-5xl font-ysumc">项目正在制作……</h1>
       <p class="text-4xl pb-12 pt-6">凡事总有过程，对吧。</p>
-      <PublicButton text="看看别的项目" link="/project" color="gray" />
+      <PublicButton text="看看别的项目" link="/project" />
     </div>
   </PublicHero>
 </template>
