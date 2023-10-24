6e3f6e85f1595144f5727b5d25e8dca7617f48ca
纾浚 <junical@email.cn>
2023-10-24 21:12:03 +0800

    百度统计

--- app.vue
+++ app.vue
@@ -13,6 +13,13 @@ const { $Heti } = useNuxtApp()
 onMounted(() => {
   const heti = new $Heti('.heti')
   heti.autoSpacing() // 自动进行中西文混排美化和标点挤压
+  var _hmt = _hmt || []
+  ;(function () {
+    var hm = document.createElement('script')
+    hm.src = 'https://hm.baidu.com/hm.js?fa3da49c5f4c868bc9a8ae469ba4051f'
+    var s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(hm, s)
+  })()
 })
 function isOutlink(link: string | undefined) {
   // 使用正则表达式判断是否为外链
